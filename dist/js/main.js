148bb9535d817dd6092778445e3b07f439cf873b
Kelsey Z <zahandke@gmail.com>
2018-05-16 13:37:09 -0400

    stuff

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,13 @@
-'use strict';
-
-document.querySelectorAll('[data-goto]').forEach(function ($navlink) {
-    var $content = document.querySelector('.content');
-    $navlink.addEventListener('click', function () {
-        $content.setAttribute('data-show', this.getAttribute('data-goto'));
-    });
-});
+"use strict";
 
 $(".menu-icon").click(function () {
-    // console.log('helloooo');
-    $(".menu-icon").toggleClass("active");
-});
-
-$(".menu-icon").click(function () {
-    // console.log('helloooo');
-    $(".sidebar").toggleClass("active");
+	// console.log('helloooo');
+	$(".sidebar").toggleClass("active");
+	$(".menu-icon").toggleClass("active");
 });
 
 $(".menu li a").click(function () {
-    $(".menu-icon").toggleClass("active");
-    $(".sidebar").toggleClass("active");
-});
-
-$(".logo").click(function () {
-    $(".menu-icon").toggleClass("active");
-    $(".sidebar").toggleClass("active");
+	$(".menu-icon").toggleClass("active");
+	$(".sidebar").toggleClass("active");
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,17 @@
-document.querySelectorAll('[data-goto]').forEach(function ($navlink) {
-	let $content = document.querySelector('.content');
-	$navlink.addEventListener('click', function () {
-		$content.setAttribute('data-show', this.getAttribute('data-goto'))		
-	})
-})
+
    
-    $(".menu-icon").click(function(){
-        // console.log('helloooo');
-        $(".menu-icon").toggleClass("active")
-    });
+$(".menu-icon").click(function(){
+	// console.log('helloooo');
+	$(".sidebar").toggleClass("active")
+	$(".menu-icon").toggleClass("active")
+});
 
-    $(".menu-icon").click(function(){
-        // console.log('helloooo');
-        $(".sidebar").toggleClass("active")
-    });
 
-    $(".menu li a").click(function(){
-        $(".menu-icon").toggleClass("active")
-        $(".sidebar").toggleClass("active")
-    });
+$(".menu li a").click(function(){
+	$(".menu-icon").toggleClass("active")
+	$(".sidebar").toggleClass("active")
+});
 
-    $(".logo").click(function(){
-        $(".menu-icon").toggleClass("active")
-        $(".sidebar").toggleClass("active")
-    });
 
 
 
